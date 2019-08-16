99cd931984373b53ee6e6ab4caf79f6bb7bf74e8
Christoph Schröder <christoph.schroeder@helmholtz-berlin.de>
2019-08-16 15:38:12 +0200

    New upstream version 2.8.1

---
+++ src/main/java/com/cosylab/vdct/util/ComboBoxFileChooser.java
@@ -0,0 +1,110 @@
+/*
+ * Created on Aug 4, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.cosylab.vdct.util;
+
+import java.awt.Component;
+import java.awt.Frame;
+import java.awt.HeadlessException;
+import java.io.File;
+
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JRootPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileSystemView;
+
+/**
+ * @author ilist
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class ComboBoxFileChooser extends JFileChooser {
+
+	/**
+	 * 
+	 */
+	public ComboBoxFileChooser() {
+		super();
+	}
+
+	/**
+	 * @param currentDirectory currentDirectory
+	 */
+	public ComboBoxFileChooser(File currentDirectory) {
+		super(currentDirectory);
+	}
+
+	/**
+	 * @param currentDirectoryPath currentDirectoryPath
+	 */
+	public ComboBoxFileChooser(String currentDirectoryPath) {
+		super(currentDirectoryPath);
+	}
+
+	/**
+	 * @param fsv fsv
+	 */
+	public ComboBoxFileChooser(FileSystemView fsv) {
+		super(fsv);
+	}
+
+	/**
+	 * @param currentDirectory currentDirectory
+	 * @param fsv fsv
+	 */
+	public ComboBoxFileChooser(File currentDirectory, FileSystemView fsv) {
+		super(currentDirectory, fsv);
+	}
+
+	/**
+	 * @param currentDirectoryPath currentDirectoryPath
+	 * @param fsv fsv
+	 */
+	public ComboBoxFileChooser(
+		String currentDirectoryPath,
+		FileSystemView fsv) {
+		super(currentDirectoryPath, fsv);
+	}
+
+	ComboBoxFileChooserDialog dialog = null;
+	/* (non-Javadoc)
+	 * @see javax.swing.JFileChooser#createDialog(java.awt.Component)
+	 */
+	protected JDialog createDialog(Component parent) throws HeadlessException {		
+		Frame frame = parent instanceof Frame ? (Frame) parent
+					  : (Frame)SwingUtilities.getAncestorOfClass(Frame.class, parent);
+
+			String title = getUI().getDialogTitle(this);
+				getAccessibleContext().setAccessibleDescription(title);
+
+				dialog = new ComboBoxFileChooserDialog(frame, this);
+				dialog.setTitle(title);
+
+				if (JDialog.isDefaultLookAndFeelDecorated()) {
+					boolean supportsWindowDecorations = 
+					UIManager.getLookAndFeel().getSupportsWindowDecorations();
+					if (supportsWindowDecorations) {
+						dialog.getRootPane().setWindowDecorationStyle(JRootPane.FILE_CHOOSER_DIALOG);
+					}
+				}
+
+				dialog.pack();
+				dialog.setLocationRelativeTo(parent);
+			
+			return dialog;
+	}
+	
+    /**
+     *
+     * @return something
+     */
+    public javax.swing.JCheckBox getJCheckBoxAbsoluteDBD() {
+		return dialog.getJCheckBoxAbsoluteDBD();
+	}
+}
